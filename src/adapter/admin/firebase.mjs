61317da053835937f79967b4e297a10a425d85b5
Evan Carothers <evan.carothers@gmail.com>
2024-08-25 13:42:39 -0500

    throw errors if no app + fix for initializeFirestoret spread args screwing up import fn

--- src/adapter/admin/firebase.mjs
+++ src/adapter/admin/firebase.mjs
@@ -6,12 +6,14 @@ export const firestoreSymbol = Symbol();
 export function firestore(options) {
   const appName = options?.server?.app || options?.app;
   const app = getApp(appName);
+  if(!app){
+    if(appName) throw new Error(`Supplied Firebase app (${appName}) does not exist -- did you initialize it?`)
+    throw new Error('The default Firebase app does not exsist -- did you initialize it?')
+  }
   const databaseId = options?.server?.databaseId || options?.databaseId
   const firestoreOpts = {
     ...(options?.server?.preferRest ? {preferRest: true} : {}),
   }
 
-  initializeFirestore(
-    ...[app, firestoreOpts, databaseId].map(v => !!v)
-  )
+  return initializeFirestore(app, firestoreOpts, databaseId)
 }
